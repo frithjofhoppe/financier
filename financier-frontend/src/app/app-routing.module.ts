f4c99cbb3fb9c74c4c29718c852bd7f5a98eb7fe
frithjofhoppe <frithjof.hoppe@icloud.com>
2019-07-28 12:28:30 +0200

    feat(BE): Integrate auth0

--- financier-frontend/src/app/app-routing.module.ts
+++ financier-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CallbackComponent} from './authentication/callback/callback.component';
+import {LogoutComponent} from './authentication/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'callback', component: CallbackComponent},
+  {path: 'logout', component: LogoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- financier-frontend/src/app/app.module.ts
+++ financier-frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import { CallbackComponent } from './authentication/callback/callback.component';
+import { LogoutComponent } from './authentication/logout/logout.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CallbackComponent,
+    LogoutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
